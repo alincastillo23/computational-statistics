a1d03dbe71e9ec645c95af0419a511f692c2ef60
alincastillo23 <alincastillo21@hotmail.com>
2020-06-26 00:41:19 -0500

    Update distancia_test.R

--- Pruebas de independencia/distancia_test.R
+++ Pruebas de independencia/distancia_test.R
@@ -40,15 +40,39 @@ prueba.distancia <- function(x, n = 3, a = 0.3, b = 0.7, alpha = 0.05){
   l <- 1
   while(l <= length(Fei)){
     if(Fei[l] < 5){
+      pi[l + 1] <- pi[l] + pi[l + 1]
+      Foi[l + 1] <- Foi[l] + Foi[l + 1]
       Fei[l + 1] <- Fei[l] + Fei[l + 1]
+      
+      pi <- pi[-l]
+      Foi <- Foi[-l]
       Fei <- Fei[-l]
+      xi <- xi[-l]
+      
       l <- l
+      if(length(Fei) == l){
+        break
+      }
+      
     }else{
       l <- l + 1
     }
   }
   
   # Perzonalizamos para presentar.
+  if(length(xi) == n){
+    xi <- c(0:(n-1), paste0(">=", n), "Totales")
+  }else{
+    xi2 <- c(paste0("0-", xi[1]))
+    for(r in 2:length(xi)){
+      if(r == length(xi)){
+        xi <- c(xi2, paste0(">=", (xi[1] + 1)), "Totales")
+      }else{
+        xi2 <- c(xi2, paste0((xi[r -1 ] + 1), "-", xi[r]))
+      }
+    }
+  }
+  
   X2c <- ((Foi - Fei)**2)/Fei
   X2c <- c(X2c, sum(X2c))
   
@@ -58,15 +82,7 @@ prueba.distancia <- function(x, n = 3, a = 0.3, b = 0.7, alpha = 0.05){
   
   pi <- c(pi, sum(pi))
   
-  xi <- c(0:(n-1),paste0(">=", n), "Totales")
-  
   temp <- data.frame(xi, pi, Foi, Fei, X2c)
-  
-  # knitr::kable(x = temp, digits = 3, align = 'c')
-  # 
-  # rpta <- ifelse(test = sum(X2c) < qchisq(p = alpha, df = 3, lower.tail = F),
-  #                "Los números generados son independientes",
-  #                "Los números generados no son independientes")
   
   return(list(tbl = knitr::kable(x = temp, digits = 3, align = 'c'),
               rpta = ifelse(test = sum(X2c) < qchisq(p = alpha, df = 3, lower.tail = F),
@@ -77,4 +93,3 @@ prueba.distancia <- function(x, n = 3, a = 0.3, b = 0.7, alpha = 0.05){
 
 prueba.distancia(x = datos.1)
 
-knitr::kable
